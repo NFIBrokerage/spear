9b358c2d7cf6e5971d29f842511e0b143afb0dd8
Michael Davis <mcarsondavis@gmail.com>
2021-04-02 10:39:34 -0500

    add formatter rules for the rpc macro

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,6 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import_deps: [:protobuf]
+  import_deps: [:protobuf],
+  locals_without_parens: [rpc: 3]
 ]
