c3d928de552d262dd3d0647fe71e13fa39c80496
Michael Davis <mcarsondavis@gmail.com>
2021-04-14 10:40:46 -0500

    remove protobuf from formatter imports

--- .formatter.exs
+++ .formatter.exs
@@ -1,6 +1,6 @@
 # Used by "mix format"
 [
   inputs: ["{mix,.formatter}.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  import_deps: [:protobuf],
-  locals_without_parens: [rpc: 3]
+  import_deps: [],
+  locals_without_parens: []
 ]
