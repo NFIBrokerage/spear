82e080bb6bc24da894f62832346aa3749f72ceb4
Michael Davis <mcarsondavis@gmail.com>
2021-04-17 19:00:22 -0500

    revert changes to .iex.exs

--- .iex.exs
+++ .iex.exs
@@ -1,11 +1,12 @@
 make_server = fn ->
   params = [
-    connection_string: "esdb://admin:changeit@localhost:2113?tls=false&keepAliveInterval=20000&keepAliveTimeout=20000",
-    # mint_opts: [
-    #   transport_opts: [
-    #     cacertfile: Path.join([__DIR__, "certs", "ca", "ca.crt"])
-    #   ]
-    # ]
+    connection_string: "esdb://localhost:2113?tls=true",
+    credentials: {"admin", "changeit"},
+    opts: [
+      transport_opts: [
+        cacertfile: Path.join([__DIR__, "certs", "ca", "ca.crt"])
+      ]
+    ]
   ]
 
   {:ok, pid} = Spear.Connection.start_link(params)
