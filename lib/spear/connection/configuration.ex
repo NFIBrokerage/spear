9236b6f7d7a8b49a7e57c37cd7d3eb4079a9041d
Michael Davis <mcarsondavis@gmail.com>
2021-04-26 17:18:32 -0500

    implement read-only mode

--- lib/spear/connection/configuration.ex
+++ lib/spear/connection/configuration.ex
@@ -8,31 +8,44 @@ defmodule Spear.Connection.Configuration do
   * `:name` - the name of the GenServer. See `t:GenServer.name/0` for more
     information. When not provided, the spawned process is not aliased to a
     name and is only addressable through its PID.
+
   * `:connection_string` - the connection string to parse
     containing all connection information. Other options like `:host` or
     `:port` will be parsed from the connection string. If options parsed from
     the connection string are passed, they will be treated as overrides to the
     value found in the connection string. Consult the EventStoreDB
     documentation for formulating a valid connection string.
+
   * `:mint_opts` - (default: `#{inspect(@default_mint_opts)}`) a keyword
     list of options to pass to mint. The default values cannot be overridden.
     This can be useful for configuring TLS. See the
     [security guide](guides/security.md) for more information.
+
   * `:host` - (default: `"localhost"`) the host address of the EventStoreDB
+
   * `:port` - (default: `2113`) the external gRPC port of the EventStoreDB
+
   * `:tls?` - (default: `false`) whether or not to use TLS to secure the
     connection to the EventStoreDB
+
   * `:username` - (default: `"admin"`) the user to connect as
+
   * `:password` - (default: `"changeit"`) the user's password
+
   * `:keep_alive_interval` - (default: `10_000`ms - 10s) the period to send
     keep-alive pings to the EventStoreDB. Set `-1` to disable keep-alive
     checks. Should be any integer value `>= 10_000`. This option can be used
     in conjunction with `:keep_alive_timeout` to properly disconnect if the
     EventStoreDB is not responding to network traffic.
+
   * `:keep_alive_timeout` - (default: `10_000`ms - 10s) the time after sending
     a keep-alive ping when the ping will be considered unacknowledged. Used
     in conjunction with `:keep_alive_interval`. Set to `-1` to disable
     keep-alive checks. Should be any integer value `>= 10_000`.
+
+  * `:read_only?` - (default: `false`) controls whether the connection should
+    be limited to read-only functionality. The list of read-only APIs can be
+    queried with `Spear.Connection.read_apis/0`
 
   See the `Spear.Connection` module docs for more information about keep-alive.
   """
@@ -58,7 +71,8 @@ defmodule Spear.Connection.Configuration do
           keep_alive_timeout: pos_integer() | false,
           mint_opts: Keyword.t(),
           valid?: boolean(),
-          errors: Keyword.t()
+          errors: Keyword.t(),
+          read_only?: boolean()
         }
 
   defstruct scheme: :http,
@@ -71,7 +85,8 @@ defmodule Spear.Connection.Configuration do
             keep_alive_timeout: 10_000,
             mint_opts: [],
             valid?: true,
-            errors: []
+            errors: [],
+            read_only?: false
 
   @doc false
   def credentials(%__MODULE__{username: username, password: password}) do
