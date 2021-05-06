7f8e94a163a83fc1703c26b9c7cf32d113f92930
Michael Davis <mcarsondavis@gmail.com>
2021-05-06 11:58:42 -0500

    use new cert locations, pull eventstore host from env

--- .iex.exs
+++ .iex.exs
@@ -1,9 +1,11 @@
 make_server = fn ->
+  host = System.get_env("EVENTSTORE_HOST") || "localhost"
+
   secure_params = [
-    connection_string: "esdb://admin:changeit@localhost:2113?tls=true",
+    connection_string: "esdb://admin:changeit@#{host}:2113?tls=true",
     mint_opts: [
       transport_opts: [
-        cacertfile: Path.join([__DIR__, "certs", "ca", "ca.crt"])
+        cacertfile: Path.join([__DIR__, "eventstoredb", "certs", "ca", "ca.crt"])
       ]
     ]
   ]
@@ -17,37 +19,4 @@ make_server = fn ->
   pid
 end
 
-get_event_and_ack = fn conn, sub ->
-  receive do
-    %Spear.Event{} = event ->
-      :ok = Spear.ack(conn, sub, event)
-
-      event
-
-  after
-    3_000 -> :no_events
-  end
-end
-
-get_event_and_nack = fn conn, sub, action ->
-  receive do
-    %Spear.Event{} = event ->
-      :ok = Spear.nack(conn, sub, event, action: action)
-
-      event
-
-  after
-    3_000 -> :no_events
-  end
-end
-
-stream = "spear_test_stream_repeatedly"
-group = "spear_iex"
-
 conn = make_server.()
-
-# subscribe = fn ->
-#   import Spear.Filter
-#
-#   Spear.subscribe(conn, self(), :all, filter: ~f/grpc-/pt)
-# end
