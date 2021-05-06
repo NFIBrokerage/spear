29832b3c140de85c4eb1d180a367c6d7b7e43e1b
Michael Davis <michael.davis@nfiindustries.com>
2021-05-06 13:17:26 -0500

    add a docker-compose workflow (#39)

--- config/test.exs
+++ config/test.exs
@@ -2,10 +2,12 @@ import Config
 
 config :spear, Spear.Test.ClientFixture, connection_string: "esdb://localhost:2113"
 
+host = System.get_env("EVENTSTORE_HOST") || "localhost"
+
 config :spear, :config,
-  connection_string: "esdb://admin:changeit@localhost:2113?tls=true",
+  connection_string: "esdb://admin:changeit@#{host}:2113?tls=true",
   mint_opts: [
     transport_opts: [
-      cacertfile: Path.join([__DIR__ | ~w(.. certs ca ca.crt)])
+      cacertfile: Path.join([__DIR__ | ~w(.. eventstoredb certs ca ca.crt)])
     ]
   ]
