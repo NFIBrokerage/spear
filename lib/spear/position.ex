1b017d98e753112cd086e29e981c1648daca0b96
Michael Davis <mcarsondavis@gmail.com>
2021-08-27 21:17:02 -0500

    mark Spear.Position as since: 0.10.0

--- lib/spear/position.ex
+++ lib/spear/position.ex
@@ -2,6 +2,7 @@ defmodule Spear.Position do
   @moduledoc """
   A data structure representing a position in the `$all` stream
   """
+  @moduledoc since: "0.10.0"
 
   require Spear.Records.Shared, as: Shared
 
@@ -9,6 +10,7 @@ defmodule Spear.Position do
   A struct representing the prepare and commit positions for an event in the
   `$all` stream
   """
+  @typedoc since: "0.10.0"
   @type t :: %__MODULE__{commit: integer(), prepare: integer()}
 
   defstruct [:commit, :prepare]
