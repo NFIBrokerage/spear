a38f81bf15e4b134410ce30cbde42cb41d2950fe
Michael Davis <mcarsondavis@gmail.com>
2021-08-27 21:11:41 -0500

    gate DateTime.to_gregorian_seconds/1 call on elixir 1.11

--- lib/spear/records/google.ex
+++ lib/spear/records/google.ex
@@ -9,11 +9,13 @@ defmodule Spear.Records.Google do
     end
   end
 
-  def timestamp(%DateTime{} = datetime) do
-    # google timestamp is number of seconds from the first second of 1970 (smeared)
-    update_in(datetime.year, &(&1 - 1970))
-    |> DateTime.to_gregorian_seconds()
-    |> timestamp()
+  if Version.match?(System.version(), ">= 1.11.0") do
+    def timestamp(%DateTime{} = datetime) do
+      # google timestamp is number of seconds from the first second of 1970 (smeared)
+      update_in(datetime.year, &(&1 - 1970))
+      |> DateTime.to_gregorian_seconds()
+      |> timestamp()
+    end
   end
 
   def timestamp({seconds, nanos}) do
