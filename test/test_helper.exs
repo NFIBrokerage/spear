88521ba106599a91a9552ed0f45becb6af327e3f
Michael Davis <mcarsondavis@gmail.com>
2021-09-08 16:21:42 -0500

    make version check less smart, exclude from regular runs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -2,7 +2,7 @@
 ExUnit.configure(
   assert_receive_timeout: 1_000,
   refute_receive_timeout: 300,
-  exclude: [:operations, :flaky]
+  exclude: [:operations, :flaky, :version_incompatible]
 )
 
 ExUnit.start()
